8bbb4152e06661a4c0913e4da6903cce6eb444ee
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2019-10-26 15:36:40 +0200

    Just an update

--- HowToThread/Program.cs
+++ HowToThread/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using static System.Console;
+using System.Threading;
 
 namespace HowToThread
 {
     class Program
-    {
-        static void Main(string[] args)
+    {        
+        public static void MainMethod()
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 100; i++)
+            {
+                WriteLine("Main thread execution state: " + i + "%");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            WriteLine("Main thread will also be doing something I guess");
+
+            Thread t1 = new Thread(ThreadMethods.Thread1);
+            Thread t2 = new Thread(() => ThreadMethods.Thread1(5));
+
+            t1.Start();
+            t2.Start();
+            MainMethod();
+
+            t1.Join();
+            t2.Join();
         }
     }
 }
---
+++ HowToThread/ThreadMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using System.Threading;
+
+namespace HowToThread
+{
+    public class ThreadMethods
+    {
+        //zrobić konstruktor aby zczytywać numery wątków
+        public static void Thread1(int number)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                WriteLine($"Thread{number} execution state: {i}%");
+            }
+        }
+
+        public static void Thread1()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                WriteLine($"Thread execution state: {i}%");
+            }
+        }
+    }
+}
